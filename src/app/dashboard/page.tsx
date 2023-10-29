b71791aad6ff38b600d41003a4f966e95051ebc6
David Stewart <teslagraafs@gmail.com>
2023-10-29 15:59:23 -0400

    Add `/dashboard` route placeholder page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Seven Seed - Dashboard",
+	description: "Seven Seed Dashboard",
+};
+
+const DashboardPage = () => <></>;
+
+export default DashboardPage;
