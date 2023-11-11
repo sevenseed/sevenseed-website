42785b9b0616406bad4573113e354dae8d60c618
Jerome Leclanche <jerome@leclan.ch>
2023-11-12 00:48:15 +0100

    Update waitlist page

--- src/app/waitlist/page.tsx
+++ src/app/waitlist/page.tsx
@@ -10,21 +10,27 @@ const WaitlistPage = () => (
 		</div>
 		<Container className="flex flex-col items-center py-16 text-center sm:py-20 lg:py-32">
 			<h1 className="mt-6 font-display text-5xl font-extrabold text-slate-900 sm:text-6xl">
-				Seven Seed is in private beta
+				Before you start...
 			</h1>
 			<p className="mt-4 text-lg tracking-tight text-slate-700">
-				We are currently in private beta and only accept a limited number of new
-				customers. Please complete our form or send us an email at{" "}
-				<EmailLink email="contact@sevenseed.eu" /> with a description of your
-				company, we will respond as soon as possible.
+				Seven Seed is currently in restricted beta. We individually vet
+				companies before creating them.
+			</p>
+			<p className="mt-4 text-lg tracking-tight text-slate-700">
+				Please complete the following form to get started.
 			</p>
 			<Link
 				href="https://share-eu1.hsforms.com/1nvcARfYkQdCpeDXUrZblKg2dapz2"
 				target="_blank"
-				className="mt-6 text-base font-medium text-blue-600 hover:text-blue-800"
+				className="mt-6 inline-flex justify-center rounded-md py-1 px-4 text-base font-semibold tracking-tight shadow-sm focus:outline-none bg-blue-600 text-white hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 active:bg-blue-700 active:text-white/80"
 			>
-				Contact us <span aria-hidden="true">&rarr;</span>
+				Get started <span aria-hidden="true">&nbsp;&rarr;</span>
 			</Link>
+			<p className="mt-4 text-lg tracking-tight text-slate-700">
+				You can also email us at{" "}
+				<EmailLink email="contact@sevenseed.eu" className="underline" /> if you
+				have any questions.
+			</p>
 		</Container>
 	</div>
 );
