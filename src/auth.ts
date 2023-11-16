55b77c7c343a160236adc368c2763aab89712482
Adrian Pascu <mail@adipascu.ro>
2023-11-16 16:11:02 +0100

    Add `getUserOptional`

--- src/auth.ts
+++ src/auth.ts
@@ -3,37 +3,44 @@ import supabase from "./supabase";
 import { RedirectType, redirect } from "next/navigation";
 
 const getFullPath = () => {
-  const urlString = headers().get("x-url");
-  if (!urlString) return null;
-  const url = new URL(urlString);
-  return url.pathname + url.search;
+	const urlString = headers().get("x-url");
+	if (!urlString) return null;
+	const url = new URL(urlString);
+	return url.pathname + url.search;
+};
+
+const getUserOptional = async () => {
+	const { data, error } = await supabase.auth.getUser();
+	if (error) {
+		if (error.status === 401) {
+			return null;
+		} else {
+			throw new Error(error.message);
+		}
+	}
+	const user = data.user;
+	if (!user.email) {
+		throw new Error("User email is missing");
+	}
+	return {
+		id: user.id,
+		email: user.email,
+	};
 };
 
 const getUser = async () => {
-  const { data, error } = await supabase.auth.getUser();
-  if (error) {
-    if (error.status === 401) {
-      const fullPath = getFullPath();
-      if (fullPath) {
-        return redirect(
-          `/login?${new URLSearchParams({ returnTo: fullPath })}`,
-          RedirectType.replace,
-        );
-      } else {
-        return redirect("/login", RedirectType.replace);
-      }
-    } else {
-      throw new Error(error.message);
-    }
-  }
-  const user = data.user
-  if (!user.email) {
-    throw new Error("User email is missing")
-  }
-  return {
-    id: user.id,
-    email: user.email,
-  }
+	const user = await getUserOptional();
+	if (user) {
+		return user;
+	}
+	const fullPath = getFullPath();
+	if (fullPath) {
+		redirect(
+			`/login?${new URLSearchParams({ returnTo: fullPath })}`,
+			RedirectType.replace,
+		);
+	} else {
+		redirect("/login", RedirectType.replace);
+	}
 };
-
 export default getUser;
