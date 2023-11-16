0446e754077964d5edba4db2cd63ca8976959724
Adrian Pascu <mail@adipascu.ro>
2023-11-16 17:32:30 +0100

    Format code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,68 +1,62 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
-import { NextResponse, type NextRequest } from 'next/server'
-import { SUPABASE_ANON_KEY, SUPABASE_URL } from './config'
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
+import { SUPABASE_ANON_KEY, SUPABASE_URL } from "./config";
 
 export const authMiddleware = async (request: NextRequest) => {
+	const requestHeaders = new Headers(request.headers);
+	requestHeaders.set("x-url", request.url);
 
-  const requestHeaders = new Headers(request.headers);
-  requestHeaders.set('x-url', request.url);
+	let response = NextResponse.next({
+		request: {
+			headers: requestHeaders,
+		},
+	});
 
-  let response = NextResponse.next({
-    request: { 
-      headers: requestHeaders,
-    },
-  })
+	const supabase = createServerClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+		cookies: {
+			get(name: string) {
+				return request.cookies.get(name)?.value;
+			},
+			set(name: string, value: string, options: CookieOptions) {
+				request.cookies.set({
+					name,
+					value,
+					...options,
+				});
+				response = NextResponse.next({
+					request: {
+						headers: requestHeaders,
+					},
+				});
+				response.cookies.set({
+					name,
+					value,
+					...options,
+				});
+			},
+			remove(name: string, options: CookieOptions) {
+				request.cookies.set({
+					name,
+					value: "",
+					...options,
+				});
+				response = NextResponse.next({
+					request: {
+						headers: requestHeaders,
+					},
+				});
+				response.cookies.set({
+					name,
+					value: "",
+					...options,
+				});
+			},
+		},
+	});
 
-  const supabase = createServerClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY,
-    {
-      cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value,
-            ...options,
-          })
-          response = NextResponse.next({
-            request: {
-              headers: requestHeaders,
-            },
-          })
-          response.cookies.set({
-            name,
-            value,
-            ...options,
-          })
-        },
-        remove(name: string, options: CookieOptions) {
-          request.cookies.set({
-            name,
-            value: '',
-            ...options,
-          })
-          response = NextResponse.next({
-            request: {
-              headers: requestHeaders,
-            },
-          })
-          response.cookies.set({
-            name,
-            value: '',
-            ...options,
-          })
-        },
-      },
-    }
-  )
+	await supabase.auth.getSession();
 
-  await supabase.auth.getSession()
+	return response;
+};
 
-  return response
-}
-
-
-export default authMiddleware
+export default authMiddleware;
