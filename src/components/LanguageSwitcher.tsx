7a8e73191bc5cd17a6817d9586d20cec1a19c45b
Adrian Pascu <mail@adipascu.ro>
2023-11-27 17:28:41 +0100

    Add language switcher dropdown

---
+++ src/components/LanguageSwitcher.tsx
@@ -0,0 +1,29 @@
+import { locales, usePathname, useRouter } from "@/navigation";
+import { useLocale } from "next-intl";
+import { useCallback } from "react";
+
+const LanguageSwitcher = () => {
+	const currentLocale = useLocale();
+	const router = useRouter();
+	const pathname = usePathname();
+	const onChange = useCallback(
+		(e: React.ChangeEvent<HTMLSelectElement>) => {
+			router.replace(pathname, { locale: e.target.value });
+		},
+		[pathname, router],
+	);
+
+	return (
+		<div className="flex items-center justify-between">
+			<select defaultValue={currentLocale} onChange={onChange}>
+				{locales.map((locale) => (
+					<option key={locale} value={locale}>
+						{locale}
+					</option>
+				))}
+			</select>
+		</div>
+	);
+};
+
+export default LanguageSwitcher;
