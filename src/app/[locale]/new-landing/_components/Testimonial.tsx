2ac872f4c61bd980f4adb062d89401e6f340ad7a
Steve Brand <thatfanficguy@protonmail.com>
2024-05-09 02:13:31 +0200

    Overhaul `<Testimonials />`

---
+++ src/app/[locale]/new-landing/_components/Testimonial.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import { type TestimonialAuthor } from "./layout/Testimonials";
+import { useTranslations } from "next-intl";
+import { Container } from "./Container";
+import { StarRating } from "./StarRating";
+
+export default function Testimonial({ author }: { author: TestimonialAuthor }) {
+	const t = useTranslations("Testimonials");
+
+	return (
+		<Container
+			size="xs"
+			className="relative bg-slate-50 border rounded-xl py-16 mx-4"
+		>
+			<figure className="flex flex-col sm:items-center gap-y-6">
+				<StarRating />
+				<blockquote className="sm:px-8 font-display text-4xl font-medium tracking-tight text-slate-900 sm:text-center text-balance">
+					“{t(`review.${author.reviewKey}`)}”
+				</blockquote>
+				<figcaption className="flex gap-2 sm:flex-col sm:items-center sm:text-center">
+					<Image
+						className="h-12 w-12 object-cover rounded-full"
+						src={author.image}
+						alt={`Avatar of ${author.name}`}
+						width={48}
+						height={48}
+						aria-hidden
+					/>
+					<div>
+						<div className="font-medium leading-6 tracking-tight text-slate-900">
+							{author.name}
+						</div>
+						<div className="text-sm text-slate-600 text-balance leading-tight sm:leading-normal">
+							<span className="sm:block">{t(author.role)}</span>
+							<span className="sm:hidden">, </span>
+							<span>{author.company}</span>
+						</div>
+					</div>
+				</figcaption>
+			</figure>
+		</Container>
+	);
+}
--- src/app/[locale]/new-landing/_components/layout/Testimonials.tsx
+++ src/app/[locale]/new-landing/_components/layout/Testimonials.tsx
@@ -1,66 +1,44 @@
-import { type PropsWithChildren } from "react";
-import Image, { type ImageProps } from "next/image";
-import { Container } from "@/components/Container";
+import { type ImageProps } from "next/image";
 import { GridPattern } from "@/components/GridPattern";
-import { StarRating } from "../StarRating";
+import Testimonial from "../Testimonial";
 
-type TestimonialAuthor = {
+import avatarMichal from "@/images/avatars/michal-tarnowski.jpg";
+
+// * this is important to enforce because `role` is a key used in translations
+type TestimonialRole = "founder";
+
+export type TestimonialAuthor = {
 	name: string;
-	role: string;
+	role: TestimonialRole;
 	company: string;
+	reviewKey: string;
 	image: ImageProps["src"];
 };
 
-export function Testimonial({
-	id,
-	author,
-	children,
-}: PropsWithChildren<{
-	id: string;
-	author: TestimonialAuthor;
-}>) {
+const testimonials: TestimonialAuthor[] = [
+	{
+		name: "Michal Tarnowski",
+		role: "founder",
+		company: "Trifolium Belgium Consulting SRL",
+		image: avatarMichal,
+		reviewKey: "michal-tarnowski",
+	},
+];
+
+export default function Testimonials() {
 	return (
-		<aside
-			id={id}
-			aria-label={`Testimonial from ${author.name}`}
+		<section
+			id="testimonials"
+			aria-label="Testimonials"
 			className="flex flex-col items-center relative bg-slate-100 py-16 sm:py-32 -mx-8"
 		>
 			<div className="text-slate-900/10">
 				<GridPattern x="50%" patternTransform="translate(0 80)" />
 			</div>
-			<Container
-				size="xs"
-				className="relative bg-slate-50 border rounded-xl py-16 mx-4"
-			>
-				<figure className="flex flex-col sm:items-center gap-y-6">
-					<div className="text-slate-900">
-						<StarRating />
-					</div>
-					<blockquote className="sm:px-8 font-display text-4xl font-medium tracking-tight text-slate-900 sm:text-center text-balance">
-						{children}
-					</blockquote>
-					<figcaption className="flex gap-2 sm:flex-col sm:items-center sm:text-center">
-						<Image
-							className="h-12 w-12 object-cover rounded-full"
-							src={author.image}
-							alt={`Avatar of ${author.name}`}
-							width={48}
-							height={48}
-							aria-hidden
-						/>
-						<div>
-							<div className="font-medium leading-6 tracking-tight text-slate-900">
-								{author.name}
-							</div>
-							<div className="text-sm text-slate-600">
-								<span className="sm:block">{author.role}</span>
-								<span className="sm:hidden">, </span>
-								<span>{author.company}</span>
-							</div>
-						</div>
-					</figcaption>
-				</figure>
-			</Container>
-		</aside>
+
+			{testimonials.map((author) => {
+				return <Testimonial key={author.name} author={author} />;
+			})}
+		</section>
 	);
 }
