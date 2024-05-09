cdae182c74ea42d91d1899b0d76a4ac58706de10
Steve Brand <thatfanficguy@protonmail.com>
2024-05-10 01:40:53 +0200

    Update footer layout

--- src/app/[locale]/new-landing/_components/layout/Footer.tsx
+++ src/app/[locale]/new-landing/_components/layout/Footer.tsx
@@ -2,6 +2,10 @@ import { GridPattern } from "@/components/GridPattern";
 import { useTranslations } from "next-intl";
 import LinkedInIcon from "../icons/LinkedInIcon";
 import GithubIcon from "../icons/GithubIcon";
+import Image from "next/image";
+
+import logo from "@/images/logo.svg";
+import clsx from "clsx";
 
 const navigation = {
 	main: [
@@ -29,41 +33,60 @@ export default function Footer() {
 	const t = useTranslations("Footer");
 
 	return (
-		<footer className="relative pt-8 sm:pt-14 -mx-8 -mt-16">
+		<footer className="flex justify-center relative pt-32 px-8 pb-8 -mx-8 -mt-16">
 			<div className="absolute inset-x-0 top-0 h-32 text-slate-900/10 [mask-image:linear-gradient(white,transparent)]">
 				<GridPattern x="50%" />
 			</div>
-			<div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
-				<nav
-					className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12"
-					aria-label="Footer"
-				>
-					{navigation.main.map((item) => (
-						<div key={item.key} className="pb-6">
-							<a
-								href={item.href}
-								className="text-sm leading-6 text-gray-600 hover:text-gray-900"
-							>
-								{t(item.key)}
-							</a>
-						</div>
-					))}
-				</nav>
-				<div className="mt-10 flex justify-center space-x-10">
-					{navigation.social.map((item) => (
-						<a
-							key={item.key}
-							href={item.href}
-							className="text-gray-400 hover:text-gray-500"
-						>
-							<span className="sr-only">{t(item.key)}</span>
-							<item.icon />
+			<div className="flex flex-col gap-y-6 w-full max-w-3xl">
+				<div className="max-w-3xl grid sm:grid-cols-2 sm:place-content-center gap-y-8">
+					<div>
+						<a href="/">
+							<Image
+								className="w-32"
+								src={logo}
+								alt="Seven Seed logo"
+								aria-hidden
+							/>
 						</a>
-					))}
+						<span className="sr-only">Seven Seed</span>
+					</div>
+					<nav
+						className="flex flex-col sm:items-end gap-y-6"
+						aria-label="Footer"
+					>
+						<div className="flex flex-col items-start sm:items-end sm:gap-y-2">
+							{navigation.main.map((item) => (
+								<a
+									key={item.key}
+									href={item.href}
+									className={clsx(
+										"leading-6 text-gray-600 hover:text-blue-600 duration-200 p-2 -mx-2",
+										"sm:text-sm sm:p-0 sm:mx-0",
+									)}
+								>
+									{t(item.key)}
+								</a>
+							))}
+						</div>
+					</nav>
 				</div>
-				<p className="mt-10 text-center text-xs leading-5 text-gray-500">
-					&copy; {new Date().getUTCFullYear()} {t("copyright")}
-				</p>
+				<div className="flex sm:flex-row-reverse justify-between items-center flex-wrap gap-x-8 gap-y-4">
+					<p className="sm:text-xs leading-5 text-gray-500">
+						&copy; {new Date().getUTCFullYear()} {t("copyright")}
+					</p>
+					<div className="flex justify-center gap-x-6 sm:gap-x-4">
+						{navigation.social.map((item) => (
+							<a
+								key={item.key}
+								href={item.href}
+								className="text-gray-400 hover:text-gray-500"
+							>
+								<span className="sr-only">{t(item.key)}</span>
+								<item.icon />
+							</a>
+						))}
+					</div>
+				</div>
 			</div>
 		</footer>
 	);
