cac066c08e30e84dffcddafcc5a9794d09d3edd9
Steve Brand <thatfanficguy@protonmail.com>
2024-05-09 02:12:00 +0200

    Fix which `<Container />` gets restyled

--- src/app/[locale]/new-landing/_components/Container.tsx
+++ src/app/[locale]/new-landing/_components/Container.tsx
@@ -2,10 +2,10 @@ import clsx from "clsx";
 import { ComponentPropsWithoutRef } from "react";
 
 const styles = {
-	xs: "mx-auto px-4 sm:px-6 md:max-w-2xl md:px-4 lg:px-2",
-	sm: "mx-auto px-4 sm:px-6 md:max-w-2xl md:px-4 lg:px-8 lg:max-w-3xl",
-	md: "mx-auto px-4 sm:px-6 md:max-w-2xl md:px-4 lg:px-8 lg:max-w-4xl",
-	lg: "mx-auto px-4 sm:px-6 md:max-w-2xl md:px-4 lg:px-8 lg:max-w-5xl",
+	xs: "px-4 sm:px-6 md:max-w-2xl md:px-4 lg:px-2",
+	sm: "px-4 sm:px-6 md:max-w-2xl md:px-4 lg:px-8 lg:max-w-3xl",
+	md: "px-4 sm:px-6 md:max-w-2xl md:px-4 lg:px-8 lg:max-w-4xl",
+	lg: "px-4 sm:px-6 md:max-w-2xl md:px-4 lg:px-8 lg:max-w-5xl",
 };
 
 export function Container({
