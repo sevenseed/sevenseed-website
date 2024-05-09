1f315f3f0f7155428eabd75f862ac88741c82141
Steve Brand <thatfanficguy@protonmail.com>
2024-05-09 02:13:31 +0200

    Overhaul `<Testimonials />`

--- src/app/[locale]/new-landing/page.tsx
+++ src/app/[locale]/new-landing/page.tsx
@@ -1,33 +1,19 @@
 import Hero from "./_components/layout/Hero";
 import Introduction from "./_components/layout/Introduction";
 // import Footer from "./_components/layout/Footer";
+import Testimonials from "./_components/layout/Testimonials";
 
 // import { FreeChapters } from "./_components/FreeChapters";
 // import { HowItWorks } from "./_components/HowItWorks";
 // import { Pricing } from "./_components/Pricing";
 // import { Resources } from "./_components/Resources";
-import { Testimonial } from "./_components/Testimonial";
-import { useTranslations } from "next-intl";
-
-import avatarMichal from "@/images/avatars/michal-tarnowski.jpg";
 
 export default function Home() {
-	const t = useTranslations("Testimonials");
-
 	return (
 		<main className="flex flex-col gap-y-16 px-8">
 			<Hero />
 			<Introduction />
-			<Testimonial
-				id="testimonial"
-				author={{
-					name: "Michal Tarnowski",
-					role: t("authorTitle"),
-					image: avatarMichal,
-				}}
-			>
-				<p>“{t("review")}”</p>
-			</Testimonial>
+			<Testimonials />
 			{/* <HowItWorks /> */}
 			{/* <Resources /> */}
 			{/* <FreeChapters /> */}
