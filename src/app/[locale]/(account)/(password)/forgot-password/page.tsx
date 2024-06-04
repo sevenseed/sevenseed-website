fb023be2d9200bf6428e2a56e51887a6fcfde440
Steve Brand <thatfanficguy@protonmail.com>
2024-06-04 23:10:07 +0200

    Implement recovery email request

--- src/app/[locale]/(account)/(password)/forgot-password/page.tsx
+++ src/app/[locale]/(account)/(password)/forgot-password/page.tsx
@@ -2,19 +2,25 @@
 import { useCallback, useState } from "react";
 import { useTranslations } from "next-intl";
 import ButtonWithLoader from "@/components/ButtonWithLoader";
-import { useRouter } from "next/navigation";
+import { sendPasswordRecoveryEmail } from "@/api/actions/auth";
 
 const ForgotPasswordPage = () => {
-	const router = useRouter();
 	const t = useTranslations("Password");
+	const [email, setEmail] = useState("");
 	const [isSending, setIsSending] = useState(false);
+	const [hasSubmittedSuccessfully, setHasSubmittedSuccessfully] = useState(false);
+	const [errorMessage, setErrorMessage] = useState("");
 
-	const handlePasswordRecovery = useCallback(() => {
+	const handlePasswordRecovery = useCallback(async () => {
 		setIsSending(true);
-		setTimeout(() => {
-			router.push("/reset-password");
-		}, 3000);
-	}, [router]);
+
+		const { data, error } = await sendPasswordRecoveryEmail(email);
+
+		if (error) setErrorMessage(error.message);
+		if (data) setHasSubmittedSuccessfully(true);
+
+		setIsSending(false);
+	}, [email]);
 
 	return (
 		<div className="flex flex-col max-w-xl mx-auto gap-y-8 px-4 mb-8">
@@ -31,14 +37,27 @@ const ForgotPasswordPage = () => {
 					className="border rounded-lg py-2 px-2"
 					type="email"
 					placeholder="hello@example.com"
+					value={email}
+					onChange={(event) => setEmail(event.currentTarget.value)}
 				/>
 				<ButtonWithLoader
 					theme="signIn"
 					isLoaderVisible={isSending}
+					disabled={!email.length || isSending}
 					onClick={handlePasswordRecovery}
 				>
 					Recover password
 				</ButtonWithLoader>
+				{errorMessage && (
+					<div className="px-4 py-4 md:py-2 bg-red-50 border border-red-300 rounded-lg text-red-500 text-center">
+						{errorMessage}
+					</div>
+				)}
+				{hasSubmittedSuccessfully && (
+					<div className="px-4 py-4 md:py-2 bg-green-50 border border-green-300 rounded-lg text-green-500 text-center">
+						Sent! Check your inbox.
+					</div>
+				)}
 			</div>
 		</div>
 	);
