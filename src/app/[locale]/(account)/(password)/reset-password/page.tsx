192d53f918e72b1a9426435511b33879f67ee856
Steve Brand <thatfanficguy@protonmail.com>
2024-06-04 23:10:40 +0200

    Implement (incomplete) password reset

--- src/app/[locale]/(account)/(password)/reset-password/page.tsx
+++ src/app/[locale]/(account)/(password)/reset-password/page.tsx
@@ -2,14 +2,33 @@
 import { useCallback, useState } from "react";
 import { useTranslations } from "next-intl";
 import ButtonWithLoader from "@/components/ButtonWithLoader";
+import { setPassword } from "@/api/actions/auth";
+import { useRouter } from "next/navigation";
 
 const ResetPasswordPage = () => {
+	const router = useRouter();
 	const t = useTranslations("Password");
 	const [isSending, setIsSending] = useState(false);
+	const [newPassword, setNewPassword] = useState("");
+	const [hasSubmittedSuccessfully, setHasSubmittedSuccessfully] = useState(false);
+	const [errorMessage, setErrorMessage] = useState("");
 
-	const handlePasswordReset = useCallback(() => {
+	const handlePasswordReset = useCallback(async () => {
 		setIsSending(true);
-	}, []);
+
+		const { data, error } = await setPassword(newPassword);
+		console.log("🚀 ~ handlePasswordReset ~ data:", data);
+
+		if (error) setErrorMessage(error.message);
+		if (data) setHasSubmittedSuccessfully(true);
+
+		setIsSending(false);
+	}, [newPassword]);
+
+	if (hasSubmittedSuccessfully) {
+		// TODO: after redirect, display toast: "Password changed succesfully"
+		router.push("/dashboard");
+	}
 
 	return (
 		<div className="flex flex-col max-w-xl mx-auto gap-y-8 px-4 mb-8">
@@ -26,14 +45,23 @@ const ResetPasswordPage = () => {
 					className="border rounded-lg py-2 px-2"
 					type="text"
 					placeholder="••••••••"
+					value={newPassword}
+					onChange={(event) => setNewPassword(event.currentTarget.value)}
 				/>
 				<ButtonWithLoader
 					theme="signIn"
 					isLoaderVisible={isSending}
+					disabled={!newPassword.length || isSending}
 					onClick={handlePasswordReset}
 				>
 					Set new password
 				</ButtonWithLoader>
+				{errorMessage && (
+					<div className="px-4 py-4 md:py-2 bg-red-50 border border-red-300 rounded text-red-500 text-center">
+						{errorMessage}
+					</div>
+				)}
+				{/* No state for successful submission due to an immediate redirect */}
 			</div>
 		</div>
 	);
