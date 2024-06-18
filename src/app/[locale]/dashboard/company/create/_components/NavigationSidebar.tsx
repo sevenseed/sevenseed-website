18246257bbd078c0d9066d79fe2f6e2c74c6182d
Steve Brand <thatfanficguy@protonmail.com>
2024-06-19 00:53:38 +0200

    KYC form UI

--- src/app/[locale]/dashboard/company/create/_components/NavigationSidebar.tsx
+++ src/app/[locale]/dashboard/company/create/_components/NavigationSidebar.tsx
@@ -12,42 +12,40 @@ type Category = {
 };
 
 const CATEGORIES: Category[] = [
-	{ order: 0, label: "Client", forms: ["client", "clientAddress"] },
-	{ order: 1, label: "Company", forms: ["company", "companyAddress"] },
-];
+	{ order: 0, label: "Company", forms: ["company", "companyAddress"] },
+	{ order: 1, label: "Client", forms: ["kyc"] },
+].sort((a, b) => a.order - b.order);
 
 export default function NavigationSidebar() {
 	const { forms, step, setStep } = useContext(NewCompanyContext);
 
 	return (
 		<div className={styles.navigationSidebar}>
-			{[...CATEGORIES]
-				.sort((a, b) => a.order - b.order)
-				.map(({ label, forms: formIDs }) => (
-					<div className={styles.navigationSidebarCategory} key={label}>
-						<span className={styles.navigationSidebarCategoryLabel}>
-							{label}
-						</span>
-						{forms
-							.filter((form) => formIDs.includes(form.id))
-							.map((form) => {
-								const isCurrentForm = form.id === step;
-								return (
-									<button
-										key={form.id}
-										className={clsx(
-											styles.navigationSidebarStep,
-											isCurrentForm &&
-												styles.navigationSidebarStepActive,
-										)}
-										onClick={() => setStep(form.id)}
-									>
-										{form.label}
-									</button>
-								);
-							})}
-					</div>
-				))}
+			{[...CATEGORIES].map(({ label, forms: formIDs }) => (
+				<div className={styles.navigationSidebarCategory} key={label}>
+					<span className={styles.navigationSidebarCategoryLabel}>
+						{label}
+					</span>
+					{forms
+						.filter((form) => formIDs.includes(form.id))
+						.map((form) => {
+							const isCurrentForm = form.id === step;
+							return (
+								<button
+									key={form.id}
+									className={clsx(
+										styles.navigationSidebarStep,
+										isCurrentForm &&
+											styles.navigationSidebarStepActive,
+									)}
+									onClick={() => setStep(form.id)}
+								>
+									{form.label}
+								</button>
+							);
+						})}
+				</div>
+			))}
 		</div>
 	);
 }
