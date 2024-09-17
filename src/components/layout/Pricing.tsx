778299055e1731fdb497eed6c1bef39a2db90999
Steve Brand <thatfanficguy@protonmail.com>
2024-09-18 00:30:20 +0200

    Formatting

--- src/components/layout/Pricing.tsx
+++ src/components/layout/Pricing.tsx
@@ -26,25 +26,25 @@ function Plan({
 	const t = useTranslations("Pricing");
 
 	return (
-		<div className="flex-auto max-w-4xl bg-slate-50 text-slate-900 border rounded-xl">
+		<div className="max-w-4xl flex-auto border rounded-xl bg-slate-50 text-slate-900">
 			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
 				<div
 					className={clsx(
-						"flex flex-col justify-center flex-auto gap-y-2 pe-4 ps-4 py-8 text-center",
+						"flex flex-col flex-auto justify-center gap-y-2 py-8 text-center pe-4 ps-4",
 						"sm:ps-8 sm:py-4 sm:border-r sm:text-start",
 						"md:py-0",
 					)}
 				>
-					<h3 className="text-2xl font-semibold tracking-tight text-balance">
+					<h3 className="font-semibold text-2xl text-balance tracking-tight">
 						{t(nameKey)}
 					</h3>
-					<p className="tracking-tight text-slate-600 text-balance">
+					<p className="text-balance text-slate-600 tracking-tight">
 						{t(descriptionKey)}
 					</p>
 				</div>
 				<div
 					className={clsx(
-						"flex justify-center flex-auto",
+						"flex flex-auto justify-center",
 						"tracking-tight text-slate-900",
 						"p-8 border-t",
 						"md:flex md:px-4 md:col-span-1 md:border-r md:border-t-0 md:order-none",
@@ -54,7 +54,7 @@ function Plan({
 					<ul className="flex flex-col gap-y-2 sm:gap-y-1">
 						{featureKeys.map((key) => (
 							<li key={key} className="flex gap-x-1">
-								<CheckIcon className="h-6 w-6 flex-none fill-slate-600" />
+								<CheckIcon className="h-6 flex-none fill-slate-600 w-6" />
 								<span>{t(key)}</span>
 							</li>
 						))}
@@ -62,18 +62,18 @@ function Plan({
 				</div>
 				<div
 					className={clsx(
-						"flex items-center justify-center flex-auto p-8 border-t",
+						"flex flex-auto justify-center items-center p-8 border-t",
 						"sm:pe-8 sm:border-t-0",
 					)}
 				>
 					<div>
-						<div className="flex items-start font-display font-bold">
-							<span className="text-[1.75rem] leading-tight text-slate-500 self-start mt-1">
+						<div className="flex items-start font-bold font-display">
+							<span className="self-start mt-1 text-[1.75rem] text-slate-500 leading-tight">
 								€
 							</span>
 							<span className="text-7xl tracking-tight">{price}</span>
 						</div>
-						<span className="block text-slate-500 ml-6 -mt-2">
+						<span className="block -mt-2 ml-6 text-slate-500">
 							HVAT — one-time fee
 						</span>
 					</div>
@@ -93,10 +93,10 @@ export default function Pricing() {
 			className="max-w-3xl flex flex-col justify-center gap-y-8 mx-auto"
 		>
 			<div className="max-w-2xl flex flex-col gap-y-6">
-				<p className="font-display text-5xl font-extrabold tracking-tight text-slate-900 sm:text-6xl">
+				<p className="font-display font-extrabold text-5xl text-slate-900 sm:text-6xl tracking-tight">
 					{t("sectionHeading")}
 				</p>
-				<p className="font-display text-xl font-thin tracking-tight text-slate-900">
+				<p className="font-display font-thin text-slate-900 text-xl tracking-tight">
 					{t("subDescription")}
 				</p>
 			</div>
