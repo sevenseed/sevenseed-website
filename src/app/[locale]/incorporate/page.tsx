284d0424b33b51d6a2ac608155d826ac5b0fc2ca
Enzo Grillo <enzo.grillo@ingram.tech>
2025-05-20 13:16:06 +0200

    Fixed lint issue

--- src/app/[locale]/incorporate/page.tsx
+++ src/app/[locale]/incorporate/page.tsx
@@ -8,34 +8,31 @@ import Features from "@/components/layout/Features";
 import Testimonials from "@/components/layout/Testimonials";
 import Pricing from "@/components/layout/Pricing";
 import GetStarted from "@/components/layout/GetStarted";
-
-// Import all your messages
 import enMessages from "../../../../messages/en.json";
 import frMessages from "../../../../messages/fr.json";
 import nlMessages from "../../../../messages/nl.json";
 
 const messagesMap = {
-  en: enMessages,
-  fr: frMessages,
-  nl: nlMessages,
+	en: enMessages,
+	fr: frMessages,
+	nl: nlMessages,
 };
 
 export default function Incorporate() {
-  const searchParams = useSearchParams();
-  const lang = searchParams.get("lang") || "en";
-const messages = messagesMap[lang as keyof typeof messagesMap] || enMessages;
+	const searchParams = useSearchParams();
+	const lang = searchParams.get("lang") || "en";
+	const messages = messagesMap[lang as keyof typeof messagesMap] || enMessages;
 
-
-  return (
-    <NextIntlClientProvider locale={lang} messages={messages}>
-      <main className="flex flex-col gap-y-16 px-8 mb-8">
-        <Hero />
-        <Introduction />
-        <Features />
-        <Testimonials />
-        <Pricing />
-        <GetStarted />
-      </main>
-    </NextIntlClientProvider>
-  );
+	return (
+		<NextIntlClientProvider locale={lang} messages={messages}>
+			<main className="flex flex-col gap-y-16 px-8 mb-8">
+				<Hero />
+				<Introduction />
+				<Features />
+				<Testimonials />
+				<Pricing />
+				<GetStarted />
+			</main>
+		</NextIntlClientProvider>
+	);
 }
