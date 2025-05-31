b90d2382de520a1b932a89f812be85bac5aa1987
Jerome Leclanche <jerome@leclan.ch>
2025-05-31 13:32:48 +0200

    Remove dashboard

--- src/app/[locale]/dashboard/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { NewCompanyContextProvider } from "@/contexts/NewCompanyContext";
-import { PropsWithChildren } from "react";
-
-export default function Layout({ children }: PropsWithChildren) {
-	return <NewCompanyContextProvider>{children}</NewCompanyContextProvider>;
-}
